52e587ef29972923eeeff9b8887bfd70cea8262d
AlexeiMo <prestigio458@gmail.com>
2024-03-27 16:39:21 +0300

    feat: solve 6th task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -119,8 +119,13 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = {};
+  return (...args) => {
+    const key = JSON.stringify(args);
+    if (!cache[key]) cache[key] = func(...args);
+    return cache[key];
+  };
 }
 
 /**
