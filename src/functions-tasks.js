563cfe1078bb097afae19acd14b7d6e0d09a881f
AlexeiMo <prestigio458@gmail.com>
2024-03-27 17:44:05 +0300

    feat: solve 10th task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -231,8 +231,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let value = startFrom;
+  return () => {
+    value += 1;
+    return value - 1;
+  };
 }
 
 module.exports = {
